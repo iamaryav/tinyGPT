5ad297bb717ea21f8f6c801c658190f1724250dd
Priya Aryav <priya.aryav@gmail.com>
2025-04-20 01:15:50 +0530

    model

---
+++ model.py
@@ -0,0 +1,16 @@
+"""
+TODO: Add proper docs
+Model class to define 
+neural network architecture
+
+"""
+
+# Single Neural network
+
+# Layer
+
+# MLP - Multiple level perceptron
+
+class Block:
+    
+    pass
