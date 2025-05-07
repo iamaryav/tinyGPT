2cc6f5944db6314e4cf72e59435a63fe27f887a4
Priya Aryav <priya.aryav@gmail.com>
2025-05-07 22:50:43 +0530

    gpt2

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ class CasualSelfAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
         # Mulit-Head attention
-        assert config.n_embed % config.n_head == 0
+        assert config.n_embd % config.n_head == 0
         # self.key = nn.Linear(config.n_embd, config.n_head, bias=False)
         # self.query = nn.Linear(config.n_embd, config.n_head, bias=False)
         # self.key = nn.Linear(config.n_embd, config.n_head, bias=False)
@@ -151,13 +151,6 @@ class GPT(nn.Module):
         if target is not None:
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)), target.view(-1))
         return logits, loss
-            
-
-
-
-
-
-
     
     @classmethod
     def from_pretrained(cls, model_type):
@@ -210,7 +203,46 @@ class GPT(nn.Module):
 
 # ------------------------------------------------------
 model = GPT.from_pretrained("gpt2")
-print("Reached here...")
+import torch
+from torch.nn import functional as F
+
+model.eval()
+model.to('cuda')
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+tokens = [15496, 11, 314, 1101, 257, 3303, 2746, 11] # "Hello, I'm a language model,"
+tokens = torch.tensor(tokens, dtype=torch.long) # (8,)
+tokens = tokens.unsqueeze(0).repeat(5, 1) # (5, 8)
+x = tokens.to('cuda')
+
+# generate!
+while x.size(1) < 30: # max_length=30
+    # forward the model to get the logits
+    with torch.no_grad():
+        logits = model(x)[0] # (B, T, vocab_size)
+        # take the logits at the last position
+        logits = logits[:, -1, :] # (B, vocab_size)
+        # get the probabilities
+        probs = F.softmax(logits, dim=-1)
+        # do top-k sampling of 50 (huggingface pipeline default)
+        # topk_probs here becomes (5, 50), topk_indices is (5, 50)
+        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+        # select a token from the top-k probabilities
+        # note: multinomial does not demand the input to sum to 1
+        ix = torch.multinomial(topk_probs, 1) # (B, 1)
+        # gather the corresponding indices
+        xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
+        # append to the sequence
+        x = torch.cat((x, xcol), dim=1)
+
+# print the generated text
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
+for i in range(5):
+    tokens = x[i, :30].tolist()
+    decoded = enc.decode(tokens)
+    print(">", decoded)
+
 
 
 
