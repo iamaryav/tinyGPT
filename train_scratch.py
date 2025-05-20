b9d52aa8b3ed8133e580775350eab85f416d4674
Priya Aryav <priya.aryav@gmail.com>
2025-05-20 23:03:56 +0530

    gpt from scratch

---
+++ train_scratch.py
@@ -0,0 +1,9 @@
+# Implementing gpt implementation from scratch
+
+
+# Transformer architechture
+# output embedding + positional embedding
+# -> Layer Norm -> Multi Headed Attention block -> Layer Norm -> Feed Forward 
+# -> Linear -> Softmax
+
+# Predict/Generate
