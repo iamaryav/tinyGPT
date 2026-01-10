58710c990c1618c3265e3650056f396058f02797
Priya Aryav <priya.aryav@gmail.com>
2026-01-10 20:58:30 +0530

    run.sh for training

--- run.sh
+++ run.sh
@@ -1,4 +1,21 @@
+#!/bin/bash
 
-# -------------------------------------------------------------------
-# python venv setup with uv
-# command -v uv &> /dev/null || 
+# install uv
+command -v uv &> /dev/null || curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# create a local virtual env .venv (if it doesn't exist)
+[-d ".venv"] || uv venv
+
+# install the dependencies
+uv sync
+
+# activate the virtual env
+source .venv/bin/activate
+
+# download the datasets
+# change dataset for different runs
+# python -m tinygpt.data.prepare_openweb
+python -m tinygpt.data.prepare_shakespeare
+
+# run the training scripts
+python -m scripts.train_qwen
